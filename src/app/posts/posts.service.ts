2af50cc19173bbd3b27e8ad1f588044ab3720793
chengr25 <384372602@qq.com>
2021-09-14 00:39:23 +0800

    lec 56.从mongoDB取回来的post对象带有_id，在这里用map的方法转换为id

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,18 +2,28 @@ import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
 import { Post } from "./post.model";
 import { HttpClient } from '@angular/common/http';
+import { map} from 'rxjs/operators'
 
 @Injectable({providedIn: 'root'})
 export class PostService {
-  private posts: Post[] = [];
+  private posts: any[] = [];
   private postsUpdated  = new Subject<Post[]>();
 
   constructor(private http: HttpClient){}
 
   getPosts(){
-    this.http.get<Post[]>("http://localhost:3000/api/posts")
-    .subscribe((posts)=>{
-      this.posts= posts;
+    this.http.get<any>("http://localhost:3000/api/posts")
+    .pipe(map((originalPosts)=>{
+      return originalPosts.map( (post: { title: string; content: string; _id: string; })=>{
+        return {
+          title: post.title,
+          content: post.content,
+          id: post._id
+        }
+      })
+    }))
+    .subscribe((transformedPosts)=>{
+      this.posts= transformedPosts;
       this.postsUpdated.next([...this.posts]);
     })
   }
