8243e0c924cc8f0aebc4dcaacd7e39a0834267df
chengr25 <384372602@qq.com>
2021-09-14 02:49:30 +0800

    lec 58.上一个commit的效果是，点击某个post的删除，页面实时显示会把所有的post都删除，再reload才是正确的只删除了目标post的结果。原因是因为arrow function写错了，除非只有一条语句，否则不能省略return

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -44,12 +44,11 @@ export class PostService {
   deletePost(postId : string){
     this.http.delete("http://localhost:3000/api/posts/" + postId)
     .subscribe(()=>{
-      const updatedPosts = this.posts.filter(post => {
-        post.id !== postId;
+      const updatedPosts = this.posts.filter( (post:Post) => {
+        return post.id !== postId;
       });
       this.posts = updatedPosts;
       this.postsUpdated.next([...this.posts]);
-      // console.log(postId + " deletede");
     })
   }
 }
