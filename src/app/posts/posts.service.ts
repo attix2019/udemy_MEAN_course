46a0137cb0243d53fe69afcfbfc6eb891c9d75af
chengr25 <384372602@qq.com>
2021-09-14 01:54:18 +0800

    lec 58.截止到这个commit，删除以后仍然没法在前端实时显示效果，得reload才能完成更新

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -44,7 +44,12 @@ export class PostService {
   deletePost(postId : string){
     this.http.delete("http://localhost:3000/api/posts/" + postId)
     .subscribe(()=>{
-      console.log(postId + " deletede");
+      const updatedPosts = this.posts.filter(post => {
+        post.id !== postId;
+      });
+      this.posts = updatedPosts;
+      this.postsUpdated.next([...this.posts]);
+      // console.log(postId + " deletede");
     })
   }
 }
