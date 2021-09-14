24919783650b554dd5f4400978e959d3b3dfefb2
chengr25 <384372602@qq.com>
2021-09-14 23:11:44 +0800

    lec 69.后端路由部分进行了整理划分

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ var express = require('express')
 var Post = require('./models/post')
 const mongoose = require('mongoose');
 
+const postRouter = require('./routes/posts')
+
 const app = express();
 //uwNLEcLJpaSrbEyX
 const uri = "mongodb+srv://cr3:uwNLEcLJpaSrbEyX@crudemy1.hjx7z.mongodb.net/crdatabase?retryWrites=true&w=majority";
@@ -22,53 +24,6 @@ app.use((req,res, next)=>{
   next();
 })
 
-app.post('/api/posts',(req,res,next)=>{
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then((responseData)=>{
-    console.log(post._id);
-    res.status(201).json(post._id);
-  });
-})
-
-app.get('/api/posts',(req,res,next)=>{
-  Post.find().then((documents)=>{
-    console.log(documents);
-    res.status(200).json(documents);
-  });
-})
-
-app.put('/api/posts/:postId',(req,res,next)=>{
-  const post = new Post( {
-    title: req.body.title,
-    content: req.body.content
-  });
-  post._id = req.params.postId;
-  Post.updateOne({_id: req.params.postId}, post).then((result)=>{
-    console.log(result);
-    res.status(200).end();
-  });
-})
-
-app.delete('/api/posts/:id', (req, res, next)=>{
-  Post.deleteOne({_id: req.params.id}).then(()=>{
-    console.log(req.params.id + " deleted");
-    res.status(200).end();
-  })
-})
-
-
-app.get('/api/posts/:id', (req, res, next)=>{
-  Post.findById( req.params.postId).then(result =>{
-    if(result){
-      console.log(result);
-      res.status(200).json(result);
-    }else{
-      res.status(404).json("not found");
-    }
-  })
-})
+app.use("/api/posts",postRouter);
 
 module.exports = app;
---
+++ backend/routes/posts.js
@@ -0,0 +1,57 @@
+const express = require('express');
+
+const Post = require('../models/post');
+
+const router = express.Router();
+
+router.post('',(req,res,next)=>{
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then((responseData)=>{
+    console.log(post._id);
+    res.status(201).json(post._id);
+  });
+})
+
+router.get('',(req,res,next)=>{
+  Post.find().then((documents)=>{
+    console.log(documents);
+    res.status(200).json(documents);
+  });
+})
+
+router.put('/:postId',(req,res,next)=>{
+  const post = new Post( {
+    title: req.body.title,
+    content: req.body.content
+  });
+  post._id = req.params.postId;
+  Post.updateOne({_id: req.params.postId}, post).then((result)=>{
+    console.log(result);
+    res.status(200).end();
+  });
+})
+
+router.delete('/:id', (req, res, next)=>{
+  Post.deleteOne({_id: req.params.id}).then(()=>{
+    console.log(req.params.id + " deleted");
+    res.status(200).end();
+  })
+})
+
+
+router.get('/:id', (req, res, next)=>{
+  Post.findById( req.params.postId).then(result =>{
+    if(result){
+      console.log(result);
+      res.status(200).json(result);
+    }else{
+      res.status(404).json("not found");
+    }
+  })
+})
+
+
+module.exports = router
