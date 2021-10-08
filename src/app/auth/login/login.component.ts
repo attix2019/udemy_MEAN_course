7d2b2b1da0ceac34eaf5efd4a6b4ff0b73134819
chengr25 <384372602@qq.com>
2021-10-08 18:50:30 +0800

    lec 96.处理login组件的用户输入. ngModel，本地引用什么的概念我还不是很清楚

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 
 @Component({
@@ -9,7 +10,7 @@ import { Component } from "@angular/core";
 export class LoginComponent {
   isLoading = false;
 
-  onLogin(){
-
+  onLogin(loginForm :NgForm){
+    console.log(loginForm.value)
   }
 }
