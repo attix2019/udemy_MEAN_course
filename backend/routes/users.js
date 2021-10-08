38479f9f7f8d498c8ad47261897a190c3e873f89
chengr25 <384372602@qq.com>
2021-10-08 20:53:49 +0800

    lec 101.后端创建jwt

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const User = require('../models/user');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const user = require('../models/user');
+const jwt = require('jsonwebtoken')
 
 const router = express.Router();
 
@@ -24,6 +26,39 @@ router.post("/signup",(req, res, next)=>{
       });
     })
   });
+})
+
+
+router.post("/login", (req, res, next)=>{
+  user.findOne({email: req.body.email})
+  .then(user=>{
+    if(!user){
+      res.status(404).json({
+        message: " auth failed"
+      });
+    }
+    return bcrypt.compare(req.body.password, user.password);
+  })
+  .then(legalPass =>{
+    if(!legalPass){
+      res.status(404).json({
+        message: " auth failed"
+      });
+    }
+    // 不明白为什么下面可以用user.email这个值，user不是前一个.then里的参数吗
+    const token = jwt.sign(
+      {email: user.email, userId: user._id},
+      'secret_this_should_be_longer',
+      {
+        expiresIn: "1h"
+      });
+
+  })
+  .catch(err =>{
+    res.status(404).json({
+      message: " auth failed"
+    });
+  })
 })
 
 module.exports = router;
