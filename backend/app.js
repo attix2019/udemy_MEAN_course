4a36780cad3018718e554e430de7ea7d3b4a8c19
chengr25 <384372602@qq.com>
2021-10-09 00:06:01 +0800

    lec 105.给发出的请求添加token请求头

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use("/images", express.static(path.join("backend/images")) );
 app.use((req,res, next)=>{
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers",
-  "Origin,X-Requested-With,Content-type,Accept");
+  "Origin,X-Requested-With,Content-type,Accept,Authorization");
   res.setHeader("Access-Control-Allow-Methods","GET,POST,DELETE,PUT");
   next();
 })
