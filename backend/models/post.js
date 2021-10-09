e3d51d28b21541d4ce9a2e1b83de5f75cacc975d
chengr25 <384372602@qq.com>
2021-10-09 16:52:51 +0800

    lec 115.鉴权部分。给post模型添加作者字段

--- backend/models/post.js
+++ backend/models/post.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose')
 var postSchema = mongoose.Schema({
   title: {type: String, required: true},
   content: {type: String, required: true},
-  imagePath : {type:String, required: true}
+  imagePath : {type:String, required: true},
+  creator: {type: mongoose.Schema.Types.ObjectId, required: true, ref: "User"}
 })
 
 module.exports = mongoose.model('Post',postSchema)
